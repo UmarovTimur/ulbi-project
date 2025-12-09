9c9c30d3644a72c5d35730a0ca9dff2ee02042f6
umarovtimur <umarovtimur2005@gmail.com>
2025-12-10 02:33:48 +0500

    init 2.1

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import App from "app/App";
+import ThemeProvider from "app/providers/ThemeProvider/ui/ThemeProvider";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+
+render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
